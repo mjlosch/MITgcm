4726a33ed3b4b59375b6c323a6af53fb757cf3ec
mjlosch <Martin.Losch@awi.de>
2020-09-18 09:47:34 +0200

    move new parameter viscFacInFw where it belongs: pkg/autodiff

--- pkg/autodiff/AUTODIFF_PARAMS.h
+++ pkg/autodiff/AUTODIFF_PARAMS.h
@@ -63,12 +63,13 @@ C       >= 1 (and useSEAICEinAdMode)    : omit pieces of seaice thermo adjoint
 
 C--   COMMON /AUTODIFF_PARM_R/ "Real" valued parameters used by the pkg.
 C     viscFacInAd :: viscosity factor for adjoint
+C     viscFacInFw :: viscosity factor for forward model
 C     SIregFacInAd :: Factor for over shoots in AD
 C     SIregFacInFw :: Factor for over shoots in FW
-      _RL viscFacInAd
+      _RL viscFacInAd, viscFacInFw
       _RL SIregFacInAd, SIregFacInFw
       COMMON /AUTODIFF_PARM_R/
-     &  viscFacInAd, SIregFacInAd, SIregFacInFw
+     &  viscFacInAd, viscFacInFw, SIregFacInAd, SIregFacInFw
 
 C--   COMMON /AUTODIFF_PARM_C/ Character valued parameters used by the pkg.
 
